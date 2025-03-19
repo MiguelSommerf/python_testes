a9d8284562f2df079cbdd7ca575272843ab0da99
Miguel Sommerfeld <miguel.sommerfeldluiz@gmail.com>
2025-03-19 15:20:06 -0300

    Add files via upload

---
+++ teste/player.py
@@ -0,0 +1,11 @@
+import librosa as lib
+from IPython.display import Audio
+
+def playerDeAudio(endereco):
+    audio_data, sample_rate = lib.load(endereco, sr=None)
+    print(f"Taxa de amostragem: {sample_rate} Hz")
+    print(f"Dados do áudio: {audio_data}")
+
+    #Mostra se o tipo do áudio é mono ou estéreo
+    print(f"Formato do array de áudio: {audio_data.shape}")
+    return Audio(audio_data, rate = sample_rate)
